04c1bd842319e14d2948c860498dd3e420c92737
Andrey Minev <andrey_st_minev@yahoo.com>
2018-06-22 18:41:03 +0300

    ве

--- NeedForSpeed/models/Garage.java
+++ NeedForSpeed/models/Garage.java
@@ -1,13 +1,15 @@
 package NeedForSpeed.models;
 
 import NeedForSpeed.models.Cars.Car;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Garage {
     private List<Car> parkedCars;
 
-    public Garage(List<Car> parkedCars) {
-        this.parkedCars = parkedCars;
+    public Garage() {
+        this.parkedCars = new ArrayList<>();
     }
 
 }
